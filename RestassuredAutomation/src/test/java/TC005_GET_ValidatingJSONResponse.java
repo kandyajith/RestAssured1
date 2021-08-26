2cc13b5371ffcd663cae3e172e828d2391a02e69
Ajith Kularathna Aluvihare Gedara <41694388+kandyajith@users.noreply.github.com>
2021-08-26 13:49:19 +0100

    Update TC005_GET_ValidatingJSONResponse.java

--- RestassuredAutomation/src/test/java/TC005_GET_ValidatingJSONResponse.java
+++ RestassuredAutomation/src/test/java/TC005_GET_ValidatingJSONResponse.java
@@ -12,24 +12,19 @@ public class TC005_GET_ValidatingJSONResponse
 	public void GetWeatherDetails()
 	{
 		  //Specify base URI
-		  RestAssured.baseURI="https://maps.googleapis.com";
+		  RestAssured.baseURI="https://restapi.demoqa.com/utilities/weather/city";
 		  
 		  //Request object
 		  RequestSpecification httpRequest=RestAssured.given();
 		  
 		  //Response object
-		  Response response=httpRequest.request(Method.GET,"/maps/api/place/nearbysearch/xml?location=-33.8670522,151.1957362&radius=1500&type=supermarket&key=AIzaSyBjGCE3VpLU4lgTqSTDmHmJ2HoELb4Jy1s");
+		  Response response=httpRequest.request(Method.GET,"/Delhi");
 		  
 		  //print response in console window
 		  String responseBody=response.getBody().asString();
 		  System.out.println("Response Body is:" +responseBody);
 		  
-		  Headers allresponse=response.headers(); //capture all the headers from response
-		  
-		  for (Header header:allheaders)
-		  {
-			  System.out.println(header.getName()+"  "+header.getValue());
-		  }
+		  Assert.asserEquals(responseBody.contains("Delhi"),ture);
 	}
 
 
